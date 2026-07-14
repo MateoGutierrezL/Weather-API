b384b791ea5431180d86d15fe2d67a245cebb915
Mateo Gutierrez Laverde <mateogutierrez7112@gmail.com>
2026-07-14 09:49:37 -0500

    weather API

---
+++ day35/main.py
@@ -0,0 +1,45 @@
+import requests 
+import smtplib
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MY_LAT = 6.244203
+MY_LONG = -75.581215
+
+api_key = os.getenv("OWN_API_KEY")
+
+parameters = {
+    "lat": MY_LAT,
+    "lon": MY_LONG,
+    "cnt": 4,
+    "appid": api_key
+}
+
+response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", params=parameters)
+response.raise_for_status()
+print(response)
+data = response.json()
+weather_id = data["list"][0]["weather"][0]["id"]
+weather_id_list = [data["list"][item]["weather"][0]["id"] for item in range(4)]
+
+will_rain = False
+
+for id in weather_id_list:
+    if id < 700:
+        will_rain = True
+        break
+
+if will_rain:
+    email = os.getenv("EMAIL_USER")
+    password = os.getenv("EMAIL_PASS")
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user= email, password=password)
+        connection.sendmail(
+            from_addr=email,
+            to_addrs=email,
+            msg=f"Subject: Get an Umbrella!!\n\nToday is going to rain so if you don't want to get wet, please bring an umbrella. Your not going to look like a gay"
+        )
